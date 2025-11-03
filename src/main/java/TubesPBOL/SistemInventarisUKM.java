c2b4369bb9e910fbb9db5eb1061e28e05c676b2b
Zahran40 <araabbil@gmail.com>
2025-11-03 21:46:13 +0700

    Initial commit

---
+++ src/main/java/TubesPBOL/SistemInventarisUKM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package TubesPBOL;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SistemInventarisUKM {
+
+    public static void main(String[] args) {
+        System.out.println("Tubes PBOL");
+    }
+}
